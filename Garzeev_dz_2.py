3c9be19f65b62a92f1e266711e1555ec81fbd493
Timofey D <timofedem@gmail.com>
2023-03-22 01:01:36 +0300

    Сделал функцию создания aerlock-ов более автоматизированой

--- Garzeev_dz_2.py
+++ Garzeev_dz_2.py
@@ -1,41 +1,47 @@
 import numpy as np
 import pygame
 import  random
-def airlocks_definition (mask):
-    #функция для генерации воздушных шлюзов на карте
+def airlocks_definition (airlocks_count,mask,matrix_len):
+    #Todone:функция для генерации воздушных шлюзов на карте
     """
     заносит на карту стартовую позицию робота и позицию выхода с корабля
     variant - переменная, содержащая в себе указание какой вариант генерации воздушного шлюза задействовать
     :param mask: матрица - карта корабля (местности)
     :return: отрисовывает на карте местоположение робота (1) и местоположение выхода с корабля (4)
     """
-    for i in range (2):
+    for i in range (airlocks_count):
         variant = random.randint(1, 4)  # определение на каком из краев карты будет находуться шлюз
         # 1- левый край карты; 2 - верхний край карты; 3 - правый край карты; 4 - нижний край карты
         if (variant==1):
-            x=random.randint(0,9)
+            x=random.randint(0,matrix_len)
+            while (mask[x][0]==1 or mask[x][0]==4):
+                x = random.randint(0, matrix_len)
             if (i==1):
                 mask[x][0]=1
             else:
                 mask[x][0]=4
         elif (variant==2):
-            y=random.randint(0,9)
+            y=random.randint(0,matrix_len)
+            while (mask[0][y]==1 or mask[0][y]==4):
+                y = random.randint(0, matrix_len)
             if (i==1):
                 mask[0][y]=1
             else:
                 mask[0][y]=4
         elif (variant==3):
-            x=random.randint(0,9)
+            x=random.randint(0,matrix_len)
             if (i==1):
-                mask[x][9]=1
+                mask[x][matrix_len]=1
             else:
-                mask[x][9]=4
+                mask[x][matrix_len]=4
         elif (variant==4):
-            y=random.randint(0,9)
+            y=random.randint(0,matrix_len)
+            while (mask[matrix_len][y]==1 or mask[matrix_len][y]==4):
+                y = random.randint(0, matrix_len)
             if (i==1):
-                mask[9][y]=1
+                mask[matrix_len][y]=1
             else:
-                mask[9][y]=4
+                mask[matrix_len][y]=4
 
 #создание карты корабля
 if __name__ == '__main__':
@@ -44,5 +50,5 @@ if __name__ == '__main__':
     #     x= random.randint(0,9)
     #     y= random.randint(0,9)
     #     mask[x][y]=2
-    airlocks_definition(mask)
+    airlocks_definition(2,mask,10-1)
     print(mask)
