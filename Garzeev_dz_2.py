fe002d82c6ecc73ca0aeba1fa7d3ebc3a0db1a32
Timofey D <timofedem@gmail.com>
2023-03-22 00:01:42 +0300

    Создал функцию для занесения на карту начальной позиции робота и шлюз через который можно покинуть корабль // Исправил ошибку со выходными значениями из функции

--- Garzeev_dz_2.py
+++ Garzeev_dz_2.py
@@ -21,21 +21,21 @@ def airlocks_definition (mask):
         elif (variant==2):
             y=random.randint(0,9)
             if (i==1):
-                mask[0][y]="1"
+                mask[0][y]=1
             else:
-                mask[0][y]="4"
+                mask[0][y]=4
         elif (variant==3):
             x=random.randint(0,9)
             if (i==1):
-                mask[x][9]="1"
+                mask[x][9]=1
             else:
-                mask[x][9]="4"
+                mask[x][9]=4
         elif (variant==4):
             y=random.randint(0,9)
             if (i==1):
-                mask[9][y]="1"
+                mask[9][y]=1
             else:
-                mask[9][y]="4"
+                mask[9][y]=4
 
 #создание карты корабля
 if __name__ == '__main__':
