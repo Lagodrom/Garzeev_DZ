a0edf5d642bf0b3d9e59eb0d07740a7bc05776a6
Timofey D <timofedem@gmail.com>
2023-03-23 02:16:03 +0300

    tmp

--- Garzeev_dz_2.py
+++ Garzeev_dz_2.py
@@ -1,3 +1,5 @@
+from typing import List, Any
+
 import numpy as np
 import pygame
 import  random
@@ -6,12 +8,12 @@ robot_cor=[0,0]
 exit_cor=[0,0]
 target_list=[]
 
-def airlocks_definition (airlocks_count,mask):
+def airlocks_definition (airlocks_count,map):
     #Todone:функция для генерации воздушных шлюзов на карте
     """
     заносит на карту стартовую позицию робота и позицию выхода с корабля
     variant - переменная, содержащая в себе указание какой вариант генерации воздушного шлюза задействовать
-    :param mask: матрица - карта корабля (местности)
+    :param map: матрица - карта корабля (местности)
     :return: отрисовывает на карте местоположение робота (1) и местоположение выхода с корабля (4)
     """
     global matrix_len
@@ -22,124 +24,157 @@ def airlocks_definition (airlocks_count,mask):
         # 1- левый край карты; 2 - верхний край карты; 3 - правый край карты; 4 - нижний край карты
         if (variant==1):
             x=random.randint(0,matrix_len)
-            while (mask[x][0]!=0):
+            while (map[x][0]!=0):
                 x = random.randint(0, matrix_len)
             if (i==0):
-                mask[x][0]=1
+                map[x][0]=1
                 robot_cor=[x,0]
             else:
-                mask[x][0]=4
+                map[x][0]=4
                 exit_cor=[x,0]
         elif (variant==2):
             y=random.randint(0,matrix_len)
-            while (mask[0][y]!=0):
+            while (map[0][y]!=0):
                 y = random.randint(0, matrix_len)
             if (i==0):
-                mask[0][y]=1
+                map[0][y]=1
                 robot_cor = [0,y]
             else:
-                mask[0][y]=4
+                map[0][y]=4
                 exit_cor = [0,y]
         elif (variant==3):
             x=random.randint(0,matrix_len)
-            while (mask[x][matrix_len]!=0):
+            while (map[x][matrix_len]!=0):
                 x = random.randint(0, matrix_len)
             if (i==0):
-                mask[x][matrix_len]=1
+                map[x][matrix_len]=1
                 robot_cor = [x,matrix_len]
             else:
-                mask[x][matrix_len]=4
+                map[x][matrix_len]=4
                 exit_cor = [x,matrix_len]
         elif (variant==4):
             y=random.randint(0,matrix_len)
-            while (mask[matrix_len][y]!=0):
+            while (map[matrix_len][y]!=0):
                 y = random.randint(0, matrix_len)
             if (i==0):
-                mask[matrix_len][y]=1
+                map[matrix_len][y]=1
                 robot_cor = [matrix_len,y]
             else:
-                mask[matrix_len][y]=4
+                map[matrix_len][y]=4
                 exit_cor = [matrix_len,y]
 
-def goal_definition (goal_count,mask):
+def goal_definition (goal_count,map):
     global matrix_len
     global target_list
     for i in range (goal_count):
             x = random.randint(0,matrix_len)
             y = random.randint(0, matrix_len)
-            while (mask[x][y]!=0):
+            while (map[x][y]!=0):
                 x = random.randint(0, matrix_len)
                 y = random.randint(0, matrix_len)
-            mask[x][y]=3
+            map[x][y]=3
             target_list.append([x,y])
-def wall_generation (wall_count,mask):
+def wall_generation (wall_count,map):
     global matrix_len
     for i in range(wall_count):
         x = random.randint(0, matrix_len)
         y = random.randint(0, matrix_len)
-        while (mask[x][y] != 0):
+        while (map[x][y] != 0):
             x = random.randint(0, matrix_len)
             y = random.randint(0, matrix_len)
-        mask[x][y] = 2
-def say_object(x,y,mask):
+        map[x][y] = 2
+def say_object(x,y,map):
     global matrix_len
     # 1 - робот.
     # 2 - стена.
     # 3 - объекты     за     которыми     едет     робот
     # 4 - зона     эвакуцию
     result = ""
-    if x > matrix_len-1 or x < 0:
+    if x > matrix_len or x < 0:
         result = "край вселенной"
-    elif y > matrix_len-1 or y < 0:
+    elif y > matrix_len or y < 0:
         result = "край вселенной"
-    elif mask[x,y] == 0:
+    elif map[x,y] == 0:
         result = "Пусто"
-    elif mask[x,y] == 2:
+    elif map[x,y] == 2:
         result = "Стена"
-    elif mask[x,y] == 3:
+    elif map[x,y] == 3:
         result = "Добыча"
-    elif mask[x,y] == 4:
+    elif map[x,y] == 4:
         result = "Выход"
     else:
         result = "НЛО!!"
 
     return result
-def what_i_see(coords,mask):
+def what_i_see(cor,map):
     global matrix_len
-    # print(coords) # [0, 9] x,y
-    for x in range(-1,2):
-        for y in range(-1,2):
-            # по сторонам света
-            # С-З | C   | C-В
-            # З   |робот| В
-            # Ю-З | Ю   | Ю-В
-            if x==0 and y==0: print("это я тут в центре стою")
-            if x==-1 and y==-1:
-                print("Ю-З вижу:",say_object(x,y))
-            if x==0 and y==-1:
-                print("Ю вижу:",say_object(x,y))
-            if x==1 and y==-1:
-                print("Ю-В вижу:",say_object(x,y))
-            if x==-1 and y==0:
-                print("З вижу:",say_object(x,y))
-            if x==1 and y==0:
-                print("В вижу:",say_object(x,y))
-            if x==-1 and y==1:
-                print("С-З вижу:",say_object(x,y))
-            if x==0 and y==1:
-                print("С вижу:",say_object(x,y))
-            if x==1 and y==1:
-                print("С-В вижу:",say_object(x,y))
+    global mask
+    x=robot_cor[0]
+    y=robot_cor[1]
+
+    print(x,y)
+    print("в комнате со мной вижу:",say_object(x, y, mask))
+    print("З вижу:",say_object(x-1, y, map))
+    print("С-З вижу:",say_object(x-1, y+1, map))
+    print("С вижу:",say_object(x, y+1, map))
+    print("С-В вижу:",say_object(x+1, y+1, map))
+    print("В вижу:",say_object(x+1, y, map))
+    print("Ю-В вижу:",say_object(x+1, y-1, map))
+    print("Ю вижу:", say_object(x, y - 1, map))
+    print("Ю-З вижу:", say_object(x - 1, y - 1, map))
+    # for x in range (-1,2):
+    #     for y in range (-1,2):
+    #         if x == -1 and y == -1:
+    #             print("Ю-З вижу:", say_object(x, y, map))
+    #         if x == 0 and y == -1:
+    #             print("Ю вижу:", say_object(x, y, map))
+    #         if x == 1 and y == -1:
+    #             print("Ю-В вижу:", say_object(x, y, map))
+    #         if x == -1 and y == 0:
+    #             print("З вижу:", say_object(x, y, map))
+    #         if x == 1 and y == 0:
+    #             print("В вижу:", say_object(x, y, map))
+    #         if x == -1 and y == 1:
+    #             print("С-З вижу:", say_object(x, y, map))
+    #         if x == 0 and y == 1:
+    #             print("С вижу:", say_object(x, y, map))
+    #         if x == 1 and y == 1:
+    #             print("С-В вижу:", say_object(x, y, map))
+    # for x in range(-1,2):
+    #     for y in range(-1,2):
+    #         # по сторонам света
+    #         # С-З | C   | C-В
+    #         # З   |робот| В
+    #         # Ю-З | Ю   | Ю-В
+    #         if x==0 and y==0: print("это я тут в центре стою")
+    #         if x==-1 and y==-1:
+    #             print("Ю-З вижу:",say_object(x,y,map))
+    #         if x==0 and y==-1:
+    #             print("Ю вижу:",say_object(x,y,map))
+    #         if x==1 and y==-1:
+    #             print("Ю-В вижу:",say_object(x,y,map))
+    #         if x==-1 and y==0:
+    #             print("З вижу:",say_object(x,y,map))
+    #         if x==1 and y==0:
+    #             print("В вижу:",say_object(x,y,map))
+    #         if x==-1 and y==1:
+    #             print("С-З вижу:",say_object(x,y,map))
+    #         if x==0 and y==1:
+    #             print("С вижу:",say_object(x,y,map))
+    #         if x==1 and y==1:
+    #             print("С-В вижу:",say_object(x,y,map))
 
     return
-def robot_move (mask):
+def robot_move (map):
     global robot_cor
     global target_list
     global exit_cor
+    global mask
     target_list.append(exit_cor)
     move=[]
     for goal_cor in target_list:
         while (robot_cor!=goal_cor):
+            what_i_see(robot_cor,map)
 #код для одного перемещения робота из сектора в сектор
             vector_move=[goal_cor[0]-robot_cor[0],goal_cor[1]-robot_cor[1]]
             if (vector_move[0]!=0):
@@ -154,56 +189,59 @@ def robot_move (mask):
                 print("попал камушек под колесо. Отклонение влево")
                 otklonenie = 1
                 if (vector_move[0]==1 and vector_move[1]==1): #отклонение влево при движение [1,1]
-                    move = [int((robot_cor[0] + vector_move[0] - otklonenie)), int((robot_cor[1] + vector_move[1]))]
-                elif (vector_move[0]==1 and vector_move[1]==-1): #отклонение влево при движение [1,-1]
-                    move = [int((robot_cor[0] + vector_move[0])), int((robot_cor[1] + vector_move[1]+otklonenie))]
-                elif (vector_move[0]==-1 and vector_move[1]==1): #отклонение влево при движение [-1,1]
-                    move = [int((robot_cor[0] + vector_move[0])), int((robot_cor[1] + vector_move[1] - otklonenie))]
-                elif (vector_move[0]==-1 and vector_move[1]==-1): #отклонение влево при движение [-1,-1]
-                    move = [int((robot_cor[0] + vector_move[0]+otklonenie)), int((robot_cor[1] + vector_move[1]))]
-                elif (vector_move[0]==1 and vector_move[1]==0): #отклонение влево при движение [1,0]
-                    move = [int((robot_cor[0] + vector_move[0])), int((robot_cor[1] + vector_move[1]+otklonenie))]
-                elif (vector_move[0]==-1 and vector_move[1]==0): #отклонение влево при движение [-1,0]
                     move = [int((robot_cor[0] + vector_move[0])), int((robot_cor[1] + vector_move[1]-otklonenie))]
-                elif (vector_move[0]==0 and vector_move[1]==1): #отклонение влево при движение [0,1]
+                elif (vector_move[0]==1 and vector_move[1]==-1): #отклонение влево при движение [1,-1]
                     move = [int((robot_cor[0] + vector_move[0]-otklonenie)), int((robot_cor[1] + vector_move[1]))]
+                elif (vector_move[0]==-1 and vector_move[1]==1): #отклонение влево при движение [-1,1]
+                    move = [int((robot_cor[0] + vector_move[0]+otklonenie)), int((robot_cor[1] + vector_move[1]))]
+                elif (vector_move[0]==-1 and vector_move[1]==-1): #отклонение влево при движение [-1,-1]
+                    move = [int((robot_cor[0] + vector_move[0])), int((robot_cor[1] + vector_move[1]+otklonenie))]
+                elif (vector_move[0]==1 and vector_move[1]==0): #отклонение влево при движение [1,0]
+                    move = [int((robot_cor[0] + vector_move[0]-otklonenie)), int((robot_cor[1] + vector_move[1]))]
+                elif (vector_move[0]==-1 and vector_move[1]==0): #отклонение влево при движение [-1,0]
+                    move = [int((robot_cor[0] + vector_move[0]+otklonenie)), int((robot_cor[1] + vector_move[1]))]
+                elif (vector_move[0]==0 and vector_move[1]==1): #отклонение влево при движение [0,1]
+                    move = [int((robot_cor[0] + vector_move[0])), int((robot_cor[1] + vector_move[1]-otklonenie))]
                 elif (vector_move[0]==0 and vector_move[1]==-1): #отклонение влево при движение [0,-1]
-                    move = [int((robot_cor[0] + vector_move[0]+otklonenie)), int((robot_cor[1] + vector_move[1]+otklonenie))]
+                    move = [int((robot_cor[0] + vector_move[0])), int((robot_cor[1] + vector_move[1]+otklonenie))]
             elif (otklonenie_chance==10):
                 print("попал камушек под колесо. Отклонение вправо")
                 otklonenie = 1
                 if (vector_move[0] == 1 and vector_move[1] == 1):  # отклонение вправо при движение [1,1]
-                    move = [int((robot_cor[0] + vector_move[0])), int((robot_cor[1] + vector_move[1]-otklonenie))]
-                elif (vector_move[0] == 1 and vector_move[1] == -1):  # отклонение вправо при движение [1,-1]
                     move = [int((robot_cor[0] + vector_move[0]-otklonenie)), int((robot_cor[1] + vector_move[1]))]
+                elif (vector_move[0] == 1 and vector_move[1] == -1):  # отклонение вправо при движение [1,-1]
+                    move = [int((robot_cor[0] + vector_move[0])), int((robot_cor[1] + vector_move[1]-otklonenie))]
                 elif (vector_move[0] == -1 and vector_move[1] == 1):  # отклонение вправо при движение [-1,1]
-                    move = [int((robot_cor[0] + vector_move[0]+otklonenie)), int((robot_cor[1] + vector_move[1]))]
+                    move = [int((robot_cor[0] + vector_move[0])), int((robot_cor[1] + vector_move[1])+otklonenie)]
                 elif (vector_move[0] == -1 and vector_move[1] == -1):  # отклонение вправо при движение [-1,-1]
-                    move = [int((robot_cor[0] + vector_move[0])), int((robot_cor[1] + vector_move[1]+otklonenie))]
+                    move = [int((robot_cor[0] + vector_move[0]-otklonenie)), int((robot_cor[1] + vector_move[1]))]
                 elif (vector_move[0] == 1 and vector_move[1] == 0):  # отклонение вправо при движение [1,0]
-                    move = [int((robot_cor[0] + vector_move[0])), int((robot_cor[1] + vector_move[1] - otklonenie))]
+                    move = [int((robot_cor[0] + vector_move[0]+otklonenie)), int((robot_cor[1] + vector_move[1]))]
                 elif (vector_move[0] == -1 and vector_move[1] == 0):  # отклонение вправо при движение [-1,0]
-                    move = [int((robot_cor[0] + vector_move[0])), int((robot_cor[1] + vector_move[1] + otklonenie))]
+                    move = [int((robot_cor[0] + vector_move[0]-otklonenie)), int((robot_cor[1] + vector_move[1]))]
                 elif (vector_move[0] == 0 and vector_move[1] == 1):  # отклонение вправо при движение [0,1]
-                    move = [int((robot_cor[0] + vector_move[0] + otklonenie)), int((robot_cor[1] + vector_move[1]))]
+                    move = [int((robot_cor[0] + vector_move[0])), int((robot_cor[1] + vector_move[1]+otklonenie))]
                 elif (vector_move[0] == 0 and vector_move[1] == -1):  # отклонение вправо при движение [0,-1]
-                    move = [int((robot_cor[0] + vector_move[0] - otklonenie)),int((robot_cor[1] + vector_move[1]))]
-            mask[robot_cor[0]][robot_cor[1]]=0
-            mask[move[0]][move[1]]=1
+                    move = [int((robot_cor[0] + vector_move[0])),int((robot_cor[1] + vector_move[1]-otklonenie))]
+            map[robot_cor[0]][robot_cor[1]]=0
+            map[move[0]][move[1]]=1
             robot_cor=[move[0],move[1]]
-            print(mask)
-        #print(mask)
+            print(map)
+        #print(map)
+    what_i_see(robot_cor, map)
 
 #@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 #создание карты корабля
 if __name__ == '__main__':
-    mask = np.zeros((10,10))
+    map = np.zeros((10,10))
     # for i in range (25):
     #     x= random.randint(0,9)
     #     y= random.randint(0,9)
-    #     mask[x][y]=2
-    airlocks_definition(2,mask)
-    goal_definition(2,mask)
-    #wall_generation(20,mask)
+    #     map[x][y]=2
+    airlocks_definition(2,map)
+    goal_definition(2,map)
+    #wall_generation(20,map)
+    mask=map
+    print(map)
+    robot_move(map)
     print(mask)
-    robot_move(mask)
